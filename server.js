58d320bd4ff9f35947ef7ab01ad8a3adec68c7d5
bharti3bk <bharti3bk@gmail.com>
2019-09-05 10:47:04 -0700

    Added logger functionality

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ server.use(logger)
 
 //custom middleware
 function logger(req, res, next) {
- console.log('logger'); 
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
  next();
 } 
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 }) 
-
 server.use('/users' , userRouter)
 
 module.exports = server;
